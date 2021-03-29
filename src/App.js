22d998e4d69705b26036a9adf43798c478b74ec4
Samuel Olorunda <Olorunda Samuel>
2021-03-29 15:53:19 +0100

    Thank You Page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Thanks from './Components/ThanksPage/Thanks';
+
+function App() {
+  return (
+   <div>
+     <Thanks />
+   </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ThanksPage/Thanks.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Thanks.css'
+import 'antd/dist/antd.css'
+import {Button} from 'antd'
+
+function Thanks() {
+  return (
+  <div className='GeneralThanksDiv'>
+   <div className='subGeneral'>
+   <h1 style={{color:'#fff', fontSize:'35px'}}>Thank You</h1>
+    <p className='theInfo'>We'll Love to bring it to your notice that all your informations are kept secret and will be attented to by necessary autorities, 
+      feel free to refer those with similar cases to this website
+    </p>
+    <strong>Lot of Love: {'</She Codes/>'}</strong>
+    <Button style={{marginTop:'20px', fontWeight:'600'}}>Home</Button>
+   </div>
+  </div>
+  );
+}
+
+export default Thanks;
